cfeabd0e6b6b4b891d7fdcc9cf1ac032d57d0d24
jasonbehik89 <jason.e.behik@gmail.com>
2018-11-14 15:01:12 +0800

    Added a CORS-enabling package and applied changes on the content security API route.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('securities', 'Api\V1\ContentSecurityController', [
-	'only' => ['index', 'show']
-]);
+Route::prefix('api')->group(function(){
+	Route::resource('v1/securities', 'Api\V1\ContentSecurityController', [
+		'only' => ['index', 'show']
+	]);
+});
