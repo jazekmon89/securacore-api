5a6613b6a303bd3bd9247b4aeb1d47d475c07e4f
jasonbehik89 <jason.e.behik@gmail.com>
2018-11-19 12:49:49 +0800

    Applied fixes on seeders with multiple row seeds

--- database/seeds/MessagesSeeder.php
+++ database/seeds/MessagesSeeder.php
@@ -12,64 +12,66 @@ class MessagesSeeder extends Seeder
     public function run()
     {
         DB::table('messages')->insert([
-            'page' => 'Banned',
-            'text' => 'You cannot continue to the website due to being banned.'
-        ],
-    	[
-    		'page' => 'Blocked',
-            'text' => 'You are prohibited to access this website due to being blocked.'
-    	],
-    	[
-    		'page' => 'Mass_Requests',
-            'text' => 'Warning, you performed too many connections.'
-    	],
-    	[
-    		'page' => 'Proxy',
-            'text' => 'Warning, Proxy detected; you are prohibited to access the website.(please disable Browser Data Compression)'
-    	],
-    	[
-    		'page' => 'Spam',
-            'text' => 'You cannot continue to the website due to Blacklisted Spammers.'
-    	],
-    	[
-    		'page' => 'Banned_Country',
-            'text' => 'Alert, Country location is banned and is prohibited to access the website'
-    	],
-    	[
-    		'page' => 'Blocked_Browser',
-            'text' => 'The browser you are using is not allowed.(Use another browser to continue)'
-    	],
-    	[
-    		'page' => 'Blocked_OS',
-            'text' => 'The Operating System you are using is not allowed.(Use another OS to continue)'
-    	],
-    	[
-    		'page' => 'Blocked_ISP',
-            'text' => 'The Internet Service Provider you are using is blacklisted; you are prohibited to access the webiste.'
-    	],
-    	[
-    		'page' => 'Blocked_RFR',
-            'text' => 'The referrer url you are using is blocked; you are prohibited to access the website.'
-    	],
-    	[
-    		'page' => 'Tor',
-            'text' => 'Alert, Tor Detected'
-    	],
-    	[
-    		'page' => 'AdBlocker',
-            'text' => 'Alert, AdBlocker detected.(please disable your AdBlocker to continue)'
-    	],
-    	[
-    		'page' => 'Sqli',
-            'text' => 'Alert, Sqli_injection detected '
-    	],
-    	[
-    		'page' => 'Fake Bot',
-            'text' => 'Alert, Fake Bot detected; you are prohibited to access the website.'
-    	],
-    	[
-    		'page' => 'Bad Bot',
-            'text' => 'Alert, Bad Bot detected; you are prohibited to access the website.'
-    	]);
+            [
+                'page' => 'Banned',
+                'text' => 'You cannot continue to the website due to being banned.'
+            ],
+            [
+                'page' => 'Blocked',
+                'text' => 'You are prohibited to access this website due to being blocked.'
+            ],
+            [
+                'page' => 'Mass_Requests',
+                'text' => 'Warning, you performed too many connections.'
+            ],
+            [
+                'page' => 'Proxy',
+                'text' => 'Warning, Proxy detected; you are prohibited to access the website.(please disable Browser Data Compression)'
+            ],
+            [
+                'page' => 'Spam',
+                'text' => 'You cannot continue to the website due to Blacklisted Spammers.'
+            ],
+            [
+                'page' => 'Banned_Country',
+                'text' => 'Alert, Country location is banned and is prohibited to access the website'
+            ],
+            [
+                'page' => 'Blocked_Browser',
+                'text' => 'The browser you are using is not allowed.(Use another browser to continue)'
+            ],
+            [
+                'page' => 'Blocked_OS',
+                'text' => 'The Operating System you are using is not allowed.(Use another OS to continue)'
+            ],
+            [
+                'page' => 'Blocked_ISP',
+                'text' => 'The Internet Service Provider you are using is blacklisted; you are prohibited to access the webiste.'
+            ],
+            [
+                'page' => 'Blocked_RFR',
+                'text' => 'The referrer url you are using is blocked; you are prohibited to access the website.'
+            ],
+            [
+                'page' => 'Tor',
+                'text' => 'Alert, Tor Detected'
+            ],
+            [
+                'page' => 'AdBlocker',
+                'text' => 'Alert, AdBlocker detected.(please disable your AdBlocker to continue)'
+            ],
+            [
+                'page' => 'Sqli',
+                'text' => 'Alert, Sqli_injection detected '
+            ],
+            [
+                'page' => 'Fake Bot',
+                'text' => 'Alert, Fake Bot detected; you are prohibited to access the website.'
+            ],
+            [
+                'page' => 'Bad Bot',
+                'text' => 'Alert, Bad Bot detected; you are prohibited to access the website.'
+            ]
+        ]);
     }
 }
--- database/seeds/SecurityLabelsSeeder.php
+++ database/seeds/SecurityLabelsSeeder.php
@@ -12,41 +12,43 @@ class SecurityLabelsSeeder extends Seeder
     public function run()
     {
         DB::table('security_labels')->insert([
-        	'name' => 'rightclick',
-        	'message' => 'Context Menu not allowed',
-        ],[
-        	'name' => 'rightclick_images',
-        	'message' => 'Context Menu on Images not allowed',
-        ],[
-        	'name' => 'cut',
-        	'message' => 'Cut not allowed',
-        ],[
-        	'name' => 'copy',
-        	'message' => 'Copy not allowed',
-        ],[
-        	'name' => 'paste',
-        	'message' => 'Paste not allowed',
-        ],[
-        	'name' => 'drag',
-        	'message' => 'Dragging is not allowed!',
-        ],[
-        	'name' => 'drop',
-        	'message' => null,
-        ],[
-        	'name' => 'printscreen',
-        	'message' => 'It is not allowed to use the Print Screen button',
-        ],[
-        	'name' => 'print',
-        	'message' => 'It is not allowed to Print',
-        ],[
-        	'name' => 'view_source',
-        	'message' => 'It is not allowed to view the source code of the site',
-        ],[
-        	'name' => 'iframe_out',
-        	'message' => null,
-        ],[
-        	'name' => 'selecting',
-        	'message' => null,
+            [
+                'name' => 'rightclick',
+                'message' => 'Context Menu not allowed'
+            ],[
+                'name' => 'rightclick_images',
+                'message' => 'Context Menu on Images not allowed'
+            ],[
+                'name' => 'cut',
+                'message' => 'Cut not allowed'
+            ],[
+                'name' => 'copy',
+                'message' => 'Copy not allowed'
+            ],[
+                'name' => 'paste',
+                'message' => 'Paste not allowed'
+            ],[
+                'name' => 'drag',
+                'message' => 'Dragging is not allowed!'
+            ],[
+                'name' => 'drop',
+                'message' => null
+            ],[
+                'name' => 'printscreen',
+                'message' => 'It is not allowed to use the Print Screen button'
+            ],[
+                'name' => 'print',
+                'message' => 'It is not allowed to Print'
+            ],[
+                'name' => 'view_source',
+                'message' => 'It is not allowed to view the source code of the site'
+            ],[
+                'name' => 'iframe_out',
+                'message' => null
+            ],[
+                'name' => 'selecting',
+                'message' => null
+            ]
         ]);
     }
 }
