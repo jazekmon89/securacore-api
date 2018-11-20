f563b196458462caeb8e6363f00428ad5c3db0ec
jasonbehik89 <jason.e.behik@gmail.com>
2018-11-20 15:57:33 +0800

    Fixes on the website scanner. Added id on the content protection api's content.

--- app/Http/Controllers/Api/V1/SecurityController.php
+++ app/Http/Controllers/Api/V1/SecurityController.php
@@ -71,6 +71,7 @@ class SecurityController extends Controller
                         ->toArray();
                     foreach ($functions as $function_id => $function) {
                         $function_security_labels = !empty($security_labels[$function_id]) ? $security_labels[$function_id] : [];
+                        $function['id'] = $function_id;
                         $function['message'] = $function_security_labels['message'];
                         $function_name = str_replace(' ', '_', $function_security_labels['name']);
                         $to_return[$security]['function'][$function_name] = $function;
