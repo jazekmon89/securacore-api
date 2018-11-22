71c13c551b84e456ec207b20bcb7a055a19b154d
jasonbehik89 <jason.e.behik@gmail.com>
2018-11-22 17:26:34 +0800

    Added back some important seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
         //$this->call(AdBlockerSettingsSeeder::class);
         //$this->call(BadBotSettingsSeeder::class);
         //$this->call(ContentSecuritySeeder::class);
-        //$this->call(DNSBLDatabasesSeeder::class);
-        //$this->call(MalwareScannerSettingsSeeder::class);
+        $this->call(DNSBLDatabasesSeeder::class);
+        $this->call(MalwareScannerSettingsSeeder::class);
         //$this->call(MassRequestsSettingsSeeder::class);
-        //$this->call(MessagesSeeder::class);
+        $this->call(MessagesSeeder::class);
         //$this->call(ProxySettingsSeeder::class);
-        //$this->call(SettingsSeeder::class);
+        $this->call(SettingsSeeder::class);
         //$this->call(SpamSettingsSeeder::class);
         //$this->call(SQLISettingsSeeder::class);
         $this->call(SecurityLabelsSeeder::class);
