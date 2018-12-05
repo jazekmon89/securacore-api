372d22607be5e1c5dcd76ffc5f0d9fb1be85c7c4
jasonbehik89 <jason.e.behik@gmail.com>
2018-12-05 17:31:01 +0800

    Resized the logo, increased website seeds, changed user id to match current user id, generated password will always have 8 or more characters.

--- database/seeds/WebsitesSeeder.php
+++ database/seeds/WebsitesSeeder.php
@@ -15,14 +15,14 @@ class WebsitesSeeder extends Seeder
     {
 
         $user_ids = DB::table('users')->pluck('id');
-        $websites = factory(Website::class, 59)->make();
+        $websites = factory(Website::class, 60)->make();
         
         foreach ($websites as $key => $website) {
-            $random_user_id = array_random(json_decode($user_ids, true));
+            //$random_user_id = array_random(json_decode($user_ids, true));
 
             $website = Website::create([
-                // 'user_id' => ($key+1),
-                'user_id' => $random_user_id,
+                'user_id' => ($key+1),
+                //'user_id' => $random_user_id,
                 'url' => $website->url,
                 'public_key' => $website->public_key,
                 'is_activated' => $website->is_activated,
